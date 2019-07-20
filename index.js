91a3f620caad0621a2c9bc92a438fa1bfdbf80e6
Valkirin <valkirinn@gmail.com>
2019-07-20 20:08:58 +0300

    tiny world 1.0

--- index.js
+++ index.js
@@ -8,6 +8,37 @@
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
 
+const dog = {
+  type: "dog",
+  name: "Rex",
+  legs: 4,
+  hands: 0,
+  say: "Woff!"
+};
+
+const cat = {
+  type: "cat",
+  name: "Buba",
+  legs: 4,
+  hands: 0,
+  say: "Meow!"
+};
+
+const woman = {
+  type: "woman",
+  name: "Katalin",
+  legs: 2,
+  hands: 2,
+  say: '"need make up my nails"'
+};
+
+const man = {
+  type: "man",
+  name: "Bob",
+  legs: 2,
+  hands: 2,
+  say: '"Everything will be done. But tomorrow."'
+};
 
 // ======== OUTPUT ========
 /* Use print(message) for output.
@@ -18,6 +49,61 @@
    so code reviewers might focus on a single file that is index.js.
    */
 
+print(
+  "Hi. I'm " +
+    dog.type +
+    " and my name is " +
+    dog.name +
+    ". I have " +
+    dog.legs +
+    " legs and " +
+    dog.hands +
+    " hands. I like to say " +
+    dog.say +
+    "."
+);
+
+print(
+  "Hi. I'm " +
+    cat.type +
+    " and my name is " +
+    cat.name +
+    ". I have " +
+    cat.legs +
+    " legs and " +
+    cat.hands +
+    " hands. I like to say " +
+    cat.say +
+    "."
+);
+
+print(
+  "Hi. I'm " +
+    woman.type +
+    " and my name is " +
+    woman.name +
+    ". I have " +
+    woman.legs +
+    " legs and " +
+    woman.hands +
+    " hands. I like to say " +
+    woman.say +
+    "."
+);
+
+print(
+  "Hi. I'm " +
+    man.type +
+    " and my name is " +
+    man.name +
+    ". I have " +
+    man.legs +
+    " legs and " +
+    man.hands +
+    " hands. I like to say " +
+    man.say +
+    "."
+);
 /* Print examples:
    print('ABC');
    print('<strong>ABC</strong>');
@@ -27,5 +113,3 @@
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
-
-
