aa6ab6028dd36eca544b8aad1aa680c41c0c362d
Valkirin <valkirinn@gmail.com>
2019-07-22 21:45:26 +0300

    code refactored & optimized

--- index.js
+++ index.js
@@ -5,111 +5,46 @@
    Web app: _put project's github pages URL here_
    */
 
+"use strict";
+
+class Inhabitant {
+  constructor(type, name, legs, hands, say) {
+    this.type = type;
+    this.name = name;
+    this.legs = legs;
+    this.hands = hands;
+    this.say = say;
+  }
+}
+
 // ======== OBJECTS DEFINITIONS ========
-// Define your objects here
 
-const dog = {
-  type: "dog",
-  name: "Rex",
-  legs: 4,
-  hands: 0,
-  say: "Woff!"
-};
+const dog = new Inhabitant("dog", "Rex", 4, 0, '"Woff!"');
 
-const cat = {
-  type: "cat",
-  name: "Buba",
-  legs: 4,
-  hands: 0,
-  say: "Meow!"
-};
+const cat = new Inhabitant("cat", "Buba", 4, 0, '"Meow!"');
 
-const woman = {
-  type: "woman",
-  name: "Katalin",
-  legs: 2,
-  hands: 2,
-  say: '"need make up my nails"'
-};
+const woman = new Inhabitant(
+  "woman",
+  "Katalin",
+  2,
+  2,
+  '"need make up my nails"'
+);
 
-const man = {
-  type: "man",
-  name: "Bob",
-  legs: 2,
-  hands: 2,
-  say: '"Everything will be done. But tomorrow."'
+const man = new Inhabitant(
+  "man",
+  "Bob",
+  2,
+  2,
+  '"Everything will be done. But tomorrow."'
+);
+
+const joinMethod = function(arg) {
+  return [arg.type, arg.name, arg.legs, arg.hands, arg.say].join(", ");
 };
 
 // ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
 
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-print(
-  "Hi. I'm " +
-    dog.type +
-    " and my name is " +
-    dog.name +
-    ". I have " +
-    dog.legs +
-    " legs and " +
-    dog.hands +
-    " hands. I like to say " +
-    dog.say +
-    "."
-);
-
-print(
-  "Hi. I'm " +
-    cat.type +
-    " and my name is " +
-    cat.name +
-    ". I have " +
-    cat.legs +
-    " legs and " +
-    cat.hands +
-    " hands. I like to say " +
-    cat.say +
-    "."
-);
-
-print(
-  "Hi. I'm " +
-    woman.type +
-    " and my name is " +
-    woman.name +
-    ". I have " +
-    woman.legs +
-    " legs and " +
-    woman.hands +
-    " hands. I like to say " +
-    woman.say +
-    "."
-);
-
-print(
-  "Hi. I'm " +
-    man.type +
-    " and my name is " +
-    man.name +
-    ". I have " +
-    man.legs +
-    " legs and " +
-    man.hands +
-    " hands. I like to say " +
-    man.say +
-    "."
-);
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+[dog, cat, woman, man].forEach(function(el) {
+  print(joinMethod(el));
+});
